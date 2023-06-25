20c7a45fc9c90b38015e7824655e1d8c591e7c44
DataDeity <73550621+DataDeity@users.noreply.github.com>
2023-06-25 23:29:25 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+
+class CustomHandler(SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.path = "Python And Html/Website projects/Website Project security/index.html"
+        return super().do_GET()
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        password = self.rfile.read(content_length).decode('utf-8')
+
+        if password == "67611":
+            response = "success"
+        else:
+            response = "failure"
+
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.send_header('Content-length', len(response))
+        self.end_headers()
+        self.wfile.write(response.encode())
+
+
+def start_http_server():
+    try:
+        server_address = ("", 8000)
+        httpd = HTTPServer(server_address, CustomHandler)
+        print(f"Server started on {httpd.server_address}")
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        httpd.server_close()
+        print("Server stopped")
+
+if __name__ == "__main__":
+    start_http_server()
